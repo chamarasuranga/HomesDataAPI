4e0b0e195c5798eeba8108f9f9d51dc506008543
chamaram <chamara.mundigala@gmail.com>
2020-01-27 12:02:24 +1100

    Added  API2 with homeController

---
+++ HomesDataAPI2/Controllers/HomesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomesDataAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HomesDataAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomesController : ControllerBase
+    {
+        private static readonly HomesData[] Homes = new[]
+        {
+            new HomesData(){ Address="1,Bristol Court ,Glen Waverley ,3150" ,Price=500000,Rooms=5 },
+            new HomesData(){ Address="1,Bristol Court ,Glen Waverley ,3150" ,Price=500000,Rooms=5 },
+            new HomesData(){ Address="1,Bristol Court ,Glen Waverley ,3150" ,Price=500000,Rooms=5 }
+        };
+
+        private readonly ILogger<HomesController> _logger;
+
+        public HomesController(ILogger<HomesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<HomesData> Get()
+        {
+            return Homes; ;
+        }
+    }
+}
---
+++ HomesDataAPI2/Models/HomesData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomesDataAPI.Models
+{
+    public class HomesData
+    {
+        public string Address { get; set; }
+        public decimal Price { get; set; }
+        public int Rooms { get; set; }
+
+
+    }
+}
